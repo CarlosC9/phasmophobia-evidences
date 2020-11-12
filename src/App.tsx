e6d23ce99b86db92b3137d2a64f0e2b9c97bfcde
Carlos <carlosicaraballo1999@gmail.com>
2020-11-12 13:15:59 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'fontsource-roboto';
+import {
+  makeStyles,
+  Theme,
+  Typography,
+  AppBar,
+  Toolbar,
+  Select,
+  FormControl,
+  InputLabel,
+  MenuItem,
+} from '@material-ui/core';
+import GhostEvidencesScreen from './screens/GhostEvidencesScreen';
+import information from './json/information.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+
+  },
+  title: {
+    flexGrow: 1,
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
+
+interface Props {
+  classes: any;
 }
 
-export default App;
+interface State {
+  language: 'english' | 'spanish';
+}
+
+class AppComponent extends Component<Props, State> {
+
+  constructor(public props: Props) {
+    super(props);
+    this.state = {
+      language: 'english',
+    }
+
+    this.languageChange = this.languageChange.bind(this);
+  }
+
+  languageChange(event: React.ChangeEvent<{ value: unknown }>) {
+    const languageEvent: any = event.target.value;
+    this.setState({
+      language: languageEvent,
+    });
+  }
+
+
+  render() {
+    const classes = this.props.classes;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              {information.text.title[this.state.language]}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <FormControl className={classes.formControl} style={{ float: "right", marginRight: '30px' }}>
+          <InputLabel id="demo-simple-select-label">{information.text.language[this.state.language]}</InputLabel>
+          <Select
+            value={this.state.language}
+            onChange={this.languageChange}
+          >
+            <MenuItem value={'spanish'}>{information.text.spanish[this.state.language]}</MenuItem>
+            <MenuItem value={'english'}>{information.text.english[this.state.language]}</MenuItem>
+          </Select>
+        </FormControl>
+        <GhostEvidencesScreen language={this.state.language}></GhostEvidencesScreen>
+      </div>
+    );
+  }
+
+  
+}
+
+export default function App() {
+  const classes = useStyles();
+  return (
+    <AppComponent classes={classes} />
+  )
+}
